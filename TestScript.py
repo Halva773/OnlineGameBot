bf078008de345a2bbb73c40dc772bd15d33695c0
Artem-HN <48379705+Halva773@users.noreply.github.com>
2021-05-05 13:48:47 +0300

    create branch test

--- TestScript.py
+++ TestScript.py
@@ -3,7 +3,7 @@ import telebot
 from telebot import types
 import random
 
-bot = telebot.TeleBot('token')
+bot = telebot.TeleBot('1795726860:AAHVQZgmXGW-ns5Xqp349yt53A-6NgefToI')
 
 def findNameWithID(id):
     with sqlite3.connect("TestTGBotDataBase.db") as con:
